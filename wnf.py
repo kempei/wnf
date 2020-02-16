a2eba95fb3d5089c0ba3013855b680f4c1196d12
EC2 Default User <ec2-user@ip-172-31-25-18.ap-northeast-1.compute.internal>
2020-02-16 04:01:19 +0000

    .text changes into get_attribute for hidden text

--- wnf.py
+++ wnf.py
@@ -68,7 +68,7 @@ class WealthNavi(Scraper):
 
             
             self.driver.get('https://invest.wealthnavi.com/service/portfolio')
-            logger.info("title: {0}".format(self.driver.title))
+            logger.debug("title: {0}".format(self.driver.title))
             trs = self.driver.find_elements_by_xpath('//*[@id="assets-class-data"]/tbody/tr')
             for tr in trs:
                 brand = self.to_brand(tr.find_element_by_tag_name('th').get_attribute("textContent")) # 銘柄
@@ -87,15 +87,16 @@ class WealthNavi(Scraper):
                 logger.debug("inserting wn_portfolio_detail for {0},{1},{2},{3},{4},{5},{6},{7}".format(log_date, brand, jpy, jpy_delta, usd, usd_delta, price_usd, qty))
             logger.info("inserted wn_portfolio_detail for {0}".format(log_date))
 
-            total_jpy = self.to_number(self.driver.find_element_by_xpath('//*[@id="content"]/div/div/section[2]/div/div/div[1]/div[1]/dl[1]/dt').text)
-            total_usd = self.to_number(self.driver.find_element_by_xpath('//*[@id="content"]/div/div/section[2]/div/div/div[1]/div[1]/dl[2]/dt').text)
-            usdrate = self.driver.find_element_by_xpath('//*[@id="assets-class-data"]/caption/span[1]').text
+            total_jpy = self.to_number(self.driver.find_element_by_xpath('//*[@id="content"]/div/div/section[2]/div/div/div[1]/div[1]/dl[1]/dt').get_attribute("textContent"))
+            total_usd = self.to_number(self.driver.find_element_by_xpath('//*[@id="content"]/div/div/section[2]/div/div/div[1]/div[1]/dl[2]/dt').get_attribute("textContent"))
+            usdrate = self.driver.find_element_by_xpath('//*[@id="assets-class-data"]/caption/span[1]').get_attribute("textContent")
             
             self.driver.get('https://invest.wealthnavi.com/service/transaction')
-            total_deposit = self.to_number(self.driver.find_element_by_xpath('//*[@class="transaction-total"]/dl[1]/dd[1]/span').text)
-            total_withdraw = self.to_number(self.driver.find_element_by_xpath('//*[@class="transaction-total"]/dl[1]/dd[2]/span').text)
+            logger.debug("title: {0}".format(self.driver.title))
+            total_deposit = self.to_number(self.driver.find_element_by_xpath('//*[@class="transaction-total"]/dl[1]/dd[1]/span').get_attribute("textContent"))
+            total_withdraw = self.to_number(self.driver.find_element_by_xpath('//*[@class="transaction-total"]/dl[1]/dd[2]/span').get_attribute("textContent"))
             
-            last_page = self.to_number(self.driver.find_element_by_xpath('//*[@id="content"]/div/div/nav/ul/li[last()]').text)
+            last_page = int(self.to_number(self.driver.find_element_by_xpath('//*[@id="content"]/div/div/nav/ul/li[last()]').get_attribute("textContent")))
             logger.info("{0} pages in transactions".format(last_page))
             
             target_type = {
@@ -112,15 +113,15 @@ class WealthNavi(Scraper):
                 self.driver.get('https://invest.wealthnavi.com/service/transaction/{0}'.format(page))
                 entry_list = self.driver.find_element_by_class_name('history-timeline').find_elements_by_tag_name('li')
                 for entry in entry_list:
-                    history_type = entry.find_element_by_class_name('assets-type').text
+                    history_type = entry.find_element_by_class_name('assets-type').get_attribute("textContent")
                     if history_type in target_type:
                         if len(entry.find_elements_by_class_name('assets-detail')) > 0:
-                            total_trade_jpy = self.to_number(entry.find_element_by_class_name('assets-detail').text)
+                            total_trade_jpy = self.to_number(entry.find_element_by_class_name('assets-detail').get_attribute("textContent"))
                         else:
                             total_trade_jpy = None
                         date_root = entry.find_element_by_class_name('date')
-                        start_date = self.to_date(date_root.find_element_by_xpath('time').text)
-                        end_date = self.to_date(date_root.find_element_by_xpath('span[2]/time').text)
+                        start_date = self.to_date(date_root.find_element_by_xpath('time').get_attribute("textContent"))
+                        end_date = self.to_date(date_root.find_element_by_xpath('span[2]/time').get_attribute("textContent"))
                         logger.debug("{0} {1}-{2} {3}".format(history_type, start_date, end_date, total_trade_jpy))
                         slack_text = ':dollar:{0}を{1}に実施しました(受取日={2})'.format(history_type, start_date.strftime('%m月%d日'), end_date.strftime('%m月%d日'))
                         if self.existance_check(con, 'wn_history', ['start_date', 'history_type'], [start_date, target_type[history_type]]):
@@ -130,17 +131,17 @@ class WealthNavi(Scraper):
                         else:
                             content_list = entry.find_element_by_class_name('history-item-content').find_elements_by_tag_name('h2')
                             for i in range(0, len(content_list)):
-                                trade_type = content_list[i].text
+                                trade_type = content_list[i].get_attribute("textContent")
                                 trs = content_list[i].find_elements_by_xpath('following-sibling::table[1]/tbody/tr')
                                 for tr in trs:
                                     spans = tr.find_elements_by_xpath('th/span')
-                                    history_brand = spans[1].text
+                                    history_brand = spans[1].get_attribute("textContent")
                                     trade_qty = spans[2].text
-                                    brand_price_usd = self.to_number(spans[3].text)
-                                    history_usdrate = spans[4].text
+                                    brand_price_usd = self.to_number(spans[3].get_attribute("textContent"))
+                                    history_usdrate = spans[4].get_attribute("textContent")
                                     spans = tr.find_elements_by_xpath('td/span')
-                                    trade_jpy = self.to_number(spans[0].text)
-                                    trade_usd = self.to_number(spans[1].text)
+                                    trade_jpy = self.to_number(spans[0].get_attribute("textContent"))
+                                    trade_usd = self.to_number(spans[1].get_attribute("textContent"))
                                     logger.debug("{0} {1} {2} {3} {4} {5}".format(history_brand, trade_qty, brand_price_usd, history_usdrate, trade_jpy, trade_usd))
                                     cur_hd.execute((start_date, target_type[history_type], target_type[trade_type], history_brand, brand_price_usd, trade_qty, trade_jpy, trade_usd))
                                     slack_text += '\n - {0}を${1}で{2}口{3}しました(合計${4})'.format(history_brand, brand_price_usd, trade_qty, trade_type, trade_usd)
