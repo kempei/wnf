39027cbbeded68b3aed536936e333b0c7a1b16aa
Igarashi <ikempei@1498773fd393.ant.amazon.com>
2021-08-26 17:50:35 +0900

    move files

---
+++ sql/sbi_ctab.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sbi_portfolio (
+    log_date DATE PRIMARY KEY,
+    usdrate NUMERIC(6,2),
+    total_amount_jpy NUMERIC(18,0),
+    total_amount_usd NUMERIC(20,2),
+    inv_capacity_jpy NUMERIC(18,0)
+);
+CREATE TABLE sbi_portfolio_detail (
+    log_date DATE,
+    brand TEXT,
+    price_usd NUMERIC(20,2),
+    qty NUMERIC(18,0),
+    amount_jpy NUMERIC(18,0),
+    amount_jpy_delta NUMERIC(18,0),
+    amount_usd NUMERIC(20,2),
+    amount_usd_delta NUMERIC(20,2),
+    PRIMARY KEY (log_date, brand)
+);
---
+++ wnf/simpleslack.py
@@ -0,0 +1,14 @@
+import slack, os
+
+class Slack():
+    def __init__(self):
+        self.slack_client = slack.WebClient(token=os.environ['SLACK_CLIENT_SECRET'])
+        self.slack_channel = os.environ['SLACK_CHANNEL']
+
+s = Slack()
+
+def send_to_slack(text):
+    response = s.slack_client.chat_postMessage(channel = s.slack_channel, text=text)
+    assert response["ok"]
+
+    
